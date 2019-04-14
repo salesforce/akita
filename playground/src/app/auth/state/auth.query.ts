9a3b9e1452f1f20869b9eb926281cabe48033552
Netanel Basal <netanel7799@gmail.com>
2019-04-14 19:19:22 +0300

    chore(playground): remove redundant code

--- playground/src/app/auth/state/auth.query.ts
+++ playground/src/app/auth/state/auth.query.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AuthState, AuthStore } from './auth.store';
 import { Query } from '@datorama/akita';
-import { combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { of } from 'rxjs/internal/observable/of';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthQuery extends Query<AuthState> {
-  isLoggedIn$ = combineLatest(this.select(user => !!user.token), of(inStorage())).pipe(map(([hasToken, inStorage]) => hasToken || inStorage));
+  isLoggedIn$ = this.select(user => !!user.token);
 
   constructor(protected store: AuthStore) {
     super(store);
   }
-}
-
-export function inStorage() {
-  const storage = JSON.parse(localStorage.getItem('AkitaStores'));
-  return storage && storage.auth;
 }
