ead8561151f97c1af831a04c9b97a6cf5f284096
Netanel Basal <netanel7799@gmail.com>
2019-06-23 10:33:04 +0300

    feat(stores): expose stores on window in browser devmode

--- akita/src/stores.ts
+++ akita/src/stores.ts
@@ -1,4 +1,10 @@
 import { Store } from './store';
+import { isDev } from './env';
+import { isBrowser } from './root';
 
 // @internal
 export const __stores__: { [storeName: string]: Store<any> } = {};
+
+if (isBrowser && isDev()) {
+  (window as any).$$stores = __stores__;
+}
