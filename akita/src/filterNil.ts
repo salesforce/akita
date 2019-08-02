35bfd445eda08a67e61856340886bc2e83d218ea
Trey Long <xealot@users.noreply.github.com>
2019-08-02 10:22:47 -0400

    Removing `null | undefined` from T return type.

--- akita/src/filterNil.ts
+++ akita/src/filterNil.ts
@@ -1,9 +1,12 @@
 import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
+// Utility type, remove types from T that are assignable to U
+type Diff<T, U> = T extends U ? never : T;
+
 /**
  * @example
  *
  * query.selectEntity(2).pipe(filterNil)
  */
-export const filterNil = <T>(source: Observable<T | undefined | null>) => source.pipe(filter((value): value is T => value !== null && typeof value !== 'undefined'));
+export const filterNil = <T>(source: Observable<T | undefined | null>) => source.pipe(filter((value): value is Diff<T, null | undefined> => value !== null && typeof value !== 'undefined'));
