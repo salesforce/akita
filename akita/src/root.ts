81f3dadc5b4951ff46a7ce01233a1bd0a55704f6
sjroesink <sjroesink@gmail.com>
2019-09-20 08:23:39 +0200

    fix(root): Fix isNotBrowser check

--- akita/src/root.ts
+++ akita/src/root.ts
@@ -2,4 +2,4 @@ export const isBrowser = typeof window !== 'undefined';
 export const isNativeScript = typeof global !== 'undefined' && typeof (<any>global).__runtimeVersion !== 'undefined';
 
 // @internal
-export const isNotBrowser = !isBrowser && !isNativeScript;
+export const isNotBrowser = !isBrowser || isNativeScript;
