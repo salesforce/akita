4c45d1287008f5b250fd0788a2e9c5438a6d2db2
sjroesink <sjroesink@gmail.com>
2019-09-27 09:49:29 +0200

    fix(persist-state): Allow in non-browser (#300)
    * fix(persistState): Allow in non-browser
    
    Allow `persistState` in non-browser environments.
    
    * fix(persist-state): add enableInNonBrowser option

--- akita/src/persistState.ts
+++ akita/src/persistState.ts
@@ -8,9 +8,9 @@ import { getValue } from './getValueByString';
 import { setAction } from './actions';
 import { setValue } from './setValueByString';
 import { $$addStore, $$deleteStore } from './dispatchers';
-import { isNotBrowser } from './root';
 import { isNil } from './isNil';
 import { isObject } from './isObject';
+import { isNotBrowser } from './root';
 
 let skipStorageUpdate = false;
 
@@ -45,6 +45,8 @@ function observify(asyncOrValue: any) {
 export interface PersistStateParams {
   /** The storage key */
   key: string;
+  /** Whether to enable persistState in a non-browser environment */
+  enableInNonBrowser: boolean;
   /** Storage strategy to use. This defaults to LocalStorage but you can pass SessionStorage or anything that implements the StorageEngine API. */
   storage: PersistStateStorage;
   /** Custom deserializer. Defaults to JSON.parse */
@@ -73,10 +75,9 @@ export interface PersistStateParams {
 }
 
 export function persistState(params?: Partial<PersistStateParams>) {
-  if (isNotBrowser) return;
-
   const defaults: PersistStateParams = {
     key: 'AkitaStores',
+    enableInNonBrowser: false,
     storage: typeof localStorage === 'undefined' ? params.storage : localStorage,
     deserialize: JSON.parse,
     serialize: JSON.stringify,
@@ -94,11 +95,13 @@ export function persistState(params?: Partial<PersistStateParams>) {
     preStorageUpdateOperator: () => source => source
   };
 
-  const { storage, deserialize, serialize, include, exclude, key, preStorageUpdate, persistOnDestroy, preStorageUpdateOperator, preStoreUpdate, skipStorageUpdate } = Object.assign(
+  const { storage, enableInNonBrowser, deserialize, serialize, include, exclude, key, preStorageUpdate, persistOnDestroy, preStorageUpdateOperator, preStoreUpdate, skipStorageUpdate } = Object.assign(
     {},
     defaults,
     params
   );
+
+  if (isNotBrowser && !enableInNonBrowser) return;
 
   const hasInclude = include.length > 0;
   const hasExclude = exclude.length > 0;
