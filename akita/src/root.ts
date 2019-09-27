46ac86ddcbff064a2bd8c6c4cf2347ca36b61f14
Sander Roesink <s.roesink@innovadis.com>
2019-09-27 09:13:13 +0200

    fix(root): restore isNativeScript

--- akita/src/root.ts
+++ akita/src/root.ts
@@ -1,2 +1,3 @@
 export const isBrowser = typeof window !== 'undefined';
 export const isNotBrowser = !isBrowser;
+export const isNativeScript = typeof global !== 'undefined' && (<any>global).__runtimeVersion !== 'undefined';
