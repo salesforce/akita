f6c6ca29674b35e09133fc7c85583eb6711b3423
Netanel Basal <netanel.basal@datorama.com>
2020-02-02 08:23:37 +0200

    fix: 🐛 snapshot manager should ignore the router store

--- akita/src/snapshotManager.ts
+++ akita/src/snapshotManager.ts
@@ -11,9 +11,11 @@ export class SnapshotManager {
     let acc = {};
     const hasInclude = stores.length > 0;
     const keys = hasInclude ? stores : Object.keys(__stores__);
-    for(let i = 0; i < keys.length; i++) {
+    for (let i = 0; i < keys.length; i++) {
       let storeName = keys[i];
-      acc[storeName] = __stores__[storeName]._value();
+      if (storeName !== 'router') {
+        acc[storeName] = __stores__[storeName]._value();
+      }
     }
 
     return acc;
@@ -22,17 +24,17 @@ export class SnapshotManager {
   /**
    * Set snapshot we get from the server
    */
-  setStoresSnapshot(stores: { [storeName: string]: any } | string, options: { skipStorageUpdate: boolean } = { skipStorageUpdate: false } ) {
+  setStoresSnapshot(stores: { [storeName: string]: any } | string, options: { skipStorageUpdate: boolean } = { skipStorageUpdate: false }) {
     options.skipStorageUpdate && setSkipStorageUpdate(true);
 
     let normalizedStores = stores;
-    if(isString(stores)) {
+    if (isString(stores)) {
       normalizedStores = JSON.parse(normalizedStores as string);
     }
 
-    for(let i = 0, keys = Object.keys(normalizedStores); i < keys.length; i++) {
+    for (let i = 0, keys = Object.keys(normalizedStores); i < keys.length; i++) {
       const storeName = keys[i];
-      if(__stores__[storeName]) {
+      if (__stores__[storeName]) {
         __stores__[storeName]._setState(() => normalizedStores[storeName]);
       }
     }
