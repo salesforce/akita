bd37f436091c964363a4550dc8501f5cd09e1e53
Daniel <dlf1987@gmail.com>
2020-03-02 15:58:03 -0500

    fix localStorage security errors

--- akita/src/root.ts
+++ akita/src/root.ts
@@ -1,3 +1,17 @@
 export const isBrowser = typeof window !== 'undefined';
 export const isNotBrowser = !isBrowser;
 export const isNativeScript = typeof global !== 'undefined' && (<any>global).__runtimeVersion !== 'undefined';
+export const hasLocalStorage = () => {
+  try {
+    return typeof localStorage !== 'undefined';
+  } catch (error) {
+    return false;
+  }
+}
+export const hasSessionStorage = () => {
+  try {
+    return typeof localStorage !== 'undefined';
+  } catch (error) {
+    return false;
+  }
+}
