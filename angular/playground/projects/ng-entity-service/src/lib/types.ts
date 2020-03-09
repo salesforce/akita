62741bb496676b72a5ab38242d2e8ea323a47294
snebjorn <eskildd@gmail.com>
2020-03-09 12:42:23 +0100

    feat(entity-service): added skip flag to skip store writes (#379)
    Also refactored to make the code more robust

--- angular/playground/projects/ng-entity-service/src/lib/types.ts
+++ angular/playground/projects/ng-entity-service/src/lib/types.ts
@@ -1,4 +1,6 @@
 import { HttpHeaders, HttpParams } from '@angular/common/http';
+import { AddEntitiesOptions } from '@datorama/akita';
+import { HttpMethod } from './ng-entity-service-notifier';
 
 export interface NgEntityServiceParams {
   baseUrl?: string;
@@ -28,3 +30,28 @@ export type HttpConfig<Entity = any> = {
   url?: string;
   mapResponseFn?: (res: any) => Entity | Entity[];
 };
+
+interface StoreWrite {
+  /**
+   * Disables writing to the store
+   *
+   * You then have to manually write to the store.
+   * This is useful when pairing the NgEntityService with the PaginatorPlugin
+   */
+  skipWrite?: boolean;
+}
+
+export type HttpGetConfig<Entity = any> = HttpConfig<Entity> & {
+  append?: boolean; // TODO fix type these are mutually exclusive
+  upsert?: boolean;
+} & StoreWrite &
+  Msg;
+
+export type HttpAddConfig<Entity = any> = HttpConfig<Entity> & Pick<AddEntitiesOptions, 'prepend'> & StoreWrite & Msg;
+
+export type HttpUpdateConfig<Entity = any> = HttpConfig<Entity> & {
+  method?: HttpMethod.PUT | HttpMethod.PATCH;
+} & StoreWrite &
+  Msg;
+
+export type HttpDeleteConfig<Entity = any> = HttpConfig<Entity> & StoreWrite & Msg;
