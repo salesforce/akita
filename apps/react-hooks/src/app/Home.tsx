cd9e634b22b455207089b0665d7d6885b331a558
Netanel Basal <netanel7799@gmail.com>
2020-04-21 13:46:01 +0300

    revert: revert app

--- apps/react-hooks/src/app/Home.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Home() {
-  return <h1>Home</h1>
-}
--- apps/react-hooks/src/app/state/todos.query.ts
+++
@@ -1,4 +0,0 @@
-import { createEntityQuery } from '@datorama/akita';
-import { TodosState, todosStore } from './todos.store';
-
-export const todosQuery = createEntityQuery<TodosState>(todosStore);
