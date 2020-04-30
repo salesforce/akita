3ae5d42c4ff5e2af2a82a45d900ab4217e14b576
Netanel Basal <netanel7799@gmail.com>
2020-04-30 08:54:21 +0300

    docs(playground): add unload to persist state

--- apps/angular-store-app/src/main.ts
+++ apps/angular-store-app/src/main.ts
@@ -1,8 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableAkitaProdMode, persistState } from '@datorama/akita';
+import { fromEvent, merge } from 'rxjs';
+import { debounceTime, mapTo, publish, switchMap } from 'rxjs/operators';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { enableAkitaProdMode, persistState } from '@datorama/akita';
 
 if (environment.production) {
   enableProdMode();
@@ -11,9 +13,21 @@ if (environment.production) {
 
 const storage = persistState({
   key: 'akitaPlayground',
-  include: ['auth.token']
+  include: ['auth.token', 'todos'],
+  preStorageUpdateOperator: () => {
+    return (source$) => {
+      return source$.pipe(
+        publish((multicasted$) => {
+          const debounced$ = multicasted$.pipe(debounceTime(2000));
+          const unload$ = multicasted$.pipe(switchMap((data) => fromEvent(window, 'unload').pipe(mapTo(data))));
+
+          return merge(debounced$, unload$);
+        })
+      );
+    };
+  },
 });
 
 platformBrowserDynamic([{ provide: 'persistStorage', useValue: storage }])
   .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
