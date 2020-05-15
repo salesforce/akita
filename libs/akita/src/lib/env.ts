6fdc25191f7037c67166a44384bcf740ff9be504
mateuszroszczyk <roszczyk93@gmail.com>
2020-05-15 06:31:45 +0200

    fix(env): don't access window outside of browser (#428)

--- libs/akita/src/lib/env.ts
+++ libs/akita/src/lib/env.ts
@@ -1,9 +1,13 @@
+import { isBrowser } from './root';
+
 export let __DEV__ = true;
 
 export function enableAkitaProdMode() {
   __DEV__ = false;
-  delete (window as any).$$stores;
-  delete (window as any).$$queries;
+  if (isBrowser) {
+    delete (window as any).$$stores;
+    delete (window as any).$$queries;
+  }
 }
 
 // @internal
