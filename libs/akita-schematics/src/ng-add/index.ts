0a0b1cb86c27d151ad6602183afb2574f2fcac9b
Netanel Basal <netanel7799@gmail.com>
2020-06-15 17:16:43 +0300

    chore(schematics): update version

--- libs/akita-schematics/src/ng-add/index.ts
+++ libs/akita-schematics/src/ng-add/index.ts
@@ -10,7 +10,7 @@ function addPackageJsonDependencies(options: Schema): Rule {
     const dependencies: NodeDependency[] = [
       {
         type: NodeDependencyType.Default,
-        version: '^4.22.3',
+        version: '^5.0.0',
         name: '@datorama/akita',
       },
     ];
