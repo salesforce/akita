a27759468d0b22fa52f74ca6f5f7073e2cff3257
Eilon Udi <8526882+Eilonudi@users.noreply.github.com>
2020-07-02 08:10:32 +0300

    feature(schematics): adding the option the create MultiActive entity store (#477)
    Co-authored-by: eilonu <eilonu@siemplify.co>

--- libs/akita-schematics/src/ng-g/entity-store/index.ts
+++ libs/akita-schematics/src/ng-g/entity-store/index.ts
@@ -1,8 +1,10 @@
-import { Rule, apply, branchAndMerge, chain, filter, mergeWith, move, noop, template, url, Tree, SchematicContext } from '@angular-devkit/schematics';
+import { apply, branchAndMerge, chain, filter, mergeWith, move, noop, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 
-import { getProjectPath, stringUtils, parseName } from '../utils';
+import { getProjectPath, parseName, stringUtils } from '../utils';
+import { EntityStoreOptions } from './models/entity-store-options.model';
+import { ActiveStateType } from './models/active-state.enum';
 
-function getExtensionState({ idType, name, withActive }: any) {
+function getExtensionState({ idType, name, withActive }: EntityStoreOptions) {
   const isID = idType === 'string' || idType === 'number';
   const entityName = name.split('/').pop();
   const entityType = stringUtils.singular(stringUtils.classify(entityName));
@@ -12,30 +14,41 @@ function getExtensionState({ idType, name, withActive }: any) {
   }
   const extensions = [`EntityState<${generics.join(', ')}>`];
   if (withActive) {
-    const activeState = isID ? `ActiveState<${idType}>` : `ActiveState`;
+    let activeState = withActive === ActiveStateType.Single ? `ActiveState` : `MultiActiveState`;
+    activeState += isID ? `<${idType}>` : ``;
     extensions.push(activeState);
   }
   return extensions.join(', ');
 }
 
-export default function(options: any): Rule {
+function getImportsString({ withActive }: EntityStoreOptions) {
+  const imports = ['EntityState', 'EntityStore', 'StoreConfig'];
+  if (withActive) {
+    imports.push(`${withActive}State`);
+  }
+  return imports.join(', ');
+}
+
+export default function (options: EntityStoreOptions): Rule {
   return (host: Tree, context: SchematicContext) => {
     options.path = getProjectPath(host, options);
 
     // Build state based on options
     const extensionState = getExtensionState(options);
+    const importsString = getImportsString(options);
 
     const parsedPath = parseName(options);
     options.name = parsedPath.name;
     options.path = parsedPath.path;
     const templateSource = apply(url('./files'), [
-      options.spec ? noop() : filter(path => !path.endsWith('.spec.ts')),
+      options.spec ? noop() : filter((path) => !path.endsWith('.spec.ts')),
       template({
         ...stringUtils,
         ...(options as object),
-        extensionState
+        extensionState,
+        importsString,
       } as any),
-      move(parsedPath.path)
+      move(parsedPath.path),
     ]);
 
     return chain([branchAndMerge(chain([mergeWith(templateSource)]))])(host, context);
---
+++ libs/akita-schematics/src/ng-g/entity-store/models/active-state.enum.ts
@@ -0,0 +1,4 @@
+export enum ActiveStateType {
+  Single = 'Active',
+  Multi = 'MultiActive',
+}
