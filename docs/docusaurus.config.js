1c2326d3162d08b8039c5e3f2c4dc89a085e2af5
Shahar Kazaz <shahar.kazaz@gmail.com>
2020-07-20 20:52:27 +0300

    docs: update docs site

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
       indexName: 'akita',
     },
     navbar: {
-      title: 'Home',
+      title: 'Akita',
       logo: {
         alt: 'Transloco',
         src: 'img/akita.svg',
@@ -24,6 +24,7 @@ module.exports = {
           activeBasePath: 'docs',
           label: 'Docs',
           position: 'left',
+          className: 'first-nav-element'
         },
         {
           to: 'sample-app',
@@ -34,13 +35,21 @@ module.exports = {
         {
           href: 'https://stackblitz.com/edit/akita-todos-app',
           label: 'Playground',
+          position: 'left',
+          className: 'header-playground-link'
+        },
+        {
+          href: 'https://gitter.im/akita-state-management/Lobby',
+          label: '',
           position: 'right',
+          className: 'header-icon-link header-gitter-link'
         },
         {
           href: 'https://github.com/datorama/akita/',
-          label: 'GitHub',
+          label: '',
           position: 'right',
-        },
+          className: 'header-icon-link header-github-link'
+        }
       ],
     },
     footer: {
@@ -94,6 +103,7 @@ module.exports = {
     },
     prism: {
       theme: require('prism-react-renderer/themes/nightOwlLight'),
+      darkTheme: require('prism-react-renderer/themes/nightOwl')
     },
     sidebarCollapsible: true,
   },
