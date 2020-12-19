d342c1f8a5413b209826c75210ab9a0925decf32
NetanelBasal <netanel7799@gmail.com>
2020-12-19 21:58:20 +0200

    feat: 🎸 update to nx 11 and ts 4

--- apps/angular-store-app/src/app/product-page/product-page.component.ts
+++ apps/angular-store-app/src/app/product-page/product-page.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { filter, map, switchMap } from 'rxjs/operators';
-import { untilDestroyed } from 'ngx-take-until-destroy';
-import { ProductsService } from '../products/state/products.service';
 import { ProductsQuery } from '../products/state/products.query';
+import { ProductsService } from '../products/state/products.service';
 
+@UntilDestroy()
 @Component({
   template: `
-    <div *ngIf="(product$ | async) as product" class="padding">
+    <div *ngIf="product$ | async as product" class="padding">
       <h1>{{ product.title }}</h1>
       <h6>{{ product.description }}</h6>
     </div>
-  `
+  `,
 })
 export class ProductPageComponent implements OnInit, OnDestroy {
   product$ = this.productsQuery.selectEntity(this.productId);
@@ -21,10 +22,10 @@ export class ProductPageComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.activatedRoute.paramMap
       .pipe(
-        map(params => params.get('id')),
-        filter(id => !this.productsQuery.hasEntity(id)),
+        map((params) => params.get('id')),
+        filter((id) => !this.productsQuery.hasEntity(id)),
         untilDestroyed(this),
-        switchMap(id => this.productsService.getProduct(id))
+        switchMap((id) => this.productsService.getProduct(id))
       )
       .subscribe();
   }
