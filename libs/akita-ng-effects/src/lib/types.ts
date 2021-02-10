b3341a12794ad0763f098959c04d26a378536262
Juri Sinitson <juri-sinitson@users.noreply.github.com>
2021-02-10 07:22:15 +0100

    docs(code): update comments of dispatch property of EffectOptions (#616)
    Co-authored-by: Juri Sinitson <juri.sinitson@omeany.de>

--- libs/akita-ng-effects/src/lib/types.ts
+++ libs/akita-ng-effects/src/lib/types.ts
@@ -1,17 +1,21 @@
 import { Subject } from 'rxjs';
 
-
 export interface EffectOptions {
+  /**
+   * This parameter dictates if the effect can dispatch an action.
+   * Set `true` if you want the effect to dispatch an action.
+   * Set to `false` or omit if the effect should not dispatch any action.
+   */
   dispatch?: boolean;
 }
 
 export interface Effect extends Subject<Action> {
   isEffect: boolean;
   name: string | null;
-  dispatchAction: boolean
+  dispatchAction: boolean;
 }
 
 export interface Action {
   type: string;
-  [key: string]: any
+  [key: string]: any;
 }
