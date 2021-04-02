5bba18d187250a328f480331d6fbe891876ca455
brett.upton <brett.upton@standardbank.co.za>
2021-04-03 00:12:39 +0200

    feat(array-utils): allow upsert and update of items with differing data

--- libs/akita/src/__tests__/arrayUpsert.spec.ts
+++ libs/akita/src/__tests__/arrayUpsert.spec.ts
@@ -3,6 +3,8 @@ import { StoreConfig } from '../lib/storeConfig';
 import { EntityStore } from '../lib/entityStore';
 import { arrayUpsert } from '../lib/arrayUpsert';
 
+type Modify<T, R> = Omit<T, keyof R> & R;
+
 interface Comment {
   id: ID;
   text: string;
@@ -34,14 +36,14 @@ describe('arrayUpsert', () => {
       title: 'title',
       comments: [
         { id: 1, text: 'comment' },
-        { id: 2, text: 'comment2' }
-      ]
+        { id: 2, text: 'comment2' },
+      ],
     };
 
     store.add(article);
 
-    store.update(1, entity => ({
-      comments: arrayUpsert(entity.comments, 1, { text: 'updated' })
+    store.update(1, (entity) => ({
+      comments: arrayUpsert(entity.comments, 1, { text: 'updated' }),
     }));
     expect(store._value().entities[1].comments[0].text).toBe('updated');
     expect(store._value().entities[1].comments.length).toBe(2);
@@ -54,14 +56,14 @@ describe('arrayUpsert', () => {
       title: 'title',
       comments: [
         { id: 1, text: 'comment' },
-        { id: 2, text: 'comment2' }
-      ]
+        { id: 2, text: 'comment2' },
+      ],
     };
 
     store.add(article);
 
-    store.update(1, entity => ({
-      comments: arrayUpsert(entity.comments, 3, { text: 'NEW' })
+    store.update(1, (entity) => ({
+      comments: arrayUpsert(entity.comments, 3, { text: 'NEW' }),
     }));
     expect(store._value().entities[1].comments[2].text).toBe('NEW');
     expect(store._value().entities[1].comments[2].id).toBe(3);
@@ -69,13 +71,123 @@ describe('arrayUpsert', () => {
     store.remove();
   });
 
+  it('should add to a non-existent collection', () => {
+    const article = {
+      id: 1,
+      title: 'title',
+      comments: [{ _id: 1, text: 'comment' } as any, { _id: 2, text: 'comment2' } as any],
+    };
+
+    store.add(article);
+
+    store.update(1, (entity) => ({
+      comments: arrayUpsert(
+        entity.comments,
+        [
+          { _id: 1, text: 'NEW1' },
+          { _id: 2, text: 'NEW2' },
+        ],
+        '_id'
+      ),
+    }));
+    expect(store._value().entities[1].comments[0].text).toBe('NEW1');
+    expect(store._value().entities[1]['comments' as any][0]._id).toBe(1);
+    expect(store._value().entities[1].comments[1].text).toBe('NEW2');
+    expect(store._value().entities[1]['comments' as any][1]._id).toBe(2);
+    expect(store._value().entities[1].comments.length).toBe(2);
+    store.remove();
+  });
+
+  it('should add many when the id is not present', () => {
+    const article: Article = {
+      id: 1,
+      title: 'title',
+      comments: [
+        { id: 1, text: 'comment' },
+        { id: 2, text: 'comment2' },
+      ],
+    };
+
+    store.add(article);
+
+    store.update(1, (entity) => ({
+      comments: arrayUpsert(entity.comments, [
+        { id: 3, text: 'NEW3' },
+        { id: 4, text: 'NEW4' },
+      ]),
+    }));
+    expect(store._value().entities[1].comments[2].text).toBe('NEW3');
+    expect(store._value().entities[1].comments[2].id).toBe(3);
+    expect(store._value().entities[1].comments[3].text).toBe('NEW4');
+    expect(store._value().entities[1].comments[3].id).toBe(4);
+    expect(store._value().entities[1].comments.length).toBe(4);
+    store.remove();
+  });
+
+  it('should update many when the id is present', () => {
+    const article: Article = {
+      id: 1,
+      title: 'title',
+      comments: [
+        { id: 1, text: 'comment' },
+        { id: 2, text: 'comment2' },
+        { id: 3, text: 'comment3' },
+      ],
+    };
+
+    store.add(article);
+
+    store.update(1, (entity) => ({
+      comments: arrayUpsert(entity.comments, [
+        { id: 1, text: 'UPDATED1' },
+        { id: 3, text: 'UPDATED3' },
+      ]),
+    }));
+    expect(store._value().entities[1].comments[0].text).toBe('UPDATED1');
+    expect(store._value().entities[1].comments[0].id).toBe(1);
+    expect(store._value().entities[1].comments[2].text).toBe('UPDATED3');
+    expect(store._value().entities[1].comments[2].id).toBe(3);
+    expect(store._value().entities[1].comments.length).toBe(3);
+    store.remove();
+  });
+
+  it('should add and update many dependant on id being present', () => {
+    const article: Article = {
+      id: 1,
+      title: 'title',
+      comments: [{ _id: 1, text: 'comment' } as any, { _id: 2, text: 'comment2' } as any, { _id: 3, text: 'comment3' } as any],
+    };
+
+    store.add(article);
+
+    store.update(1, (entity) => ({
+      comments: arrayUpsert(
+        entity.comments,
+        [
+          { _id: 1, text: 'UPDATED1' },
+          { _id: 3, text: 'UPDATED3' },
+          { _id: 4, text: 'NEW4' },
+        ],
+        '_id'
+      ),
+    }));
+    expect(store._value().entities[1].comments[0].text).toBe('UPDATED1');
+    expect(store._value().entities[1]['comments' as any][0]._id).toBe(1);
+    expect(store._value().entities[1].comments[2].text).toBe('UPDATED3');
+    expect(store._value().entities[1]['comments' as any][2]._id).toBe(3);
+    expect(store._value().entities[1].comments[3].text).toBe('NEW4');
+    expect(store._value().entities[1]['comments' as any][3]._id).toBe(4);
+    expect(store._value().entities[1].comments.length).toBe(4);
+    store.remove();
+  });
+
   it('should work with non-objects', () => {
-    store.update(state => ({
-      names: arrayUpsert(state.names, 'b', 'updatedName')
+    store.update((state) => ({
+      names: arrayUpsert(state.names, 'b', 'updatedName'),
     }));
     expect(store._value().names).toEqual(['a', 'updatedName', 'c']);
-    store.update(state => ({
-      names: arrayUpsert(state.names, 'd', 'NEW')
+    store.update((state) => ({
+      names: arrayUpsert(state.names, 'd', 'NEW'),
     }));
     expect(store._value().names).toEqual(['a', 'updatedName', 'c', 'NEW']);
   });
--- libs/akita/src/lib/arrayUpsert.ts
+++ libs/akita/src/lib/arrayUpsert.ts
@@ -3,23 +3,54 @@ import { DEFAULT_ID_KEY } from './defaultIDKey';
 import { arrayAdd } from './arrayAdd';
 import { arrayUpdate } from './arrayUpdate';
 import { isObject } from './isObject';
+import { isArray } from './isArray';
+import { coerceArray } from './coerceArray';
 
 /**
- * Upsert item in a collection
+ * Upsert item/s in a collection
  *
  * @example
  *
- *
+ * // upsert single
  * store.update(1, entity => ({
  *   comments: arrayUpsert(entity.comments, 1, { name: 'newComment' })
  * }))
+ *
+ * @example
+ *
+ * // upsert many
+ * store.update(1, entities => ({
+     comments: arrayUpsert(entities, newEntities, 'idKey')
+ }));
  */
-export function arrayUpsert<Root extends any[]>(arr: Root, id: ID, obj: Partial<Root[0]>, idKey = DEFAULT_ID_KEY): Root[0][] {
-  const entityIsObject = isObject(obj);
-  const entityExists = arr.some(entity => (entityIsObject ? entity[idKey] === id : entity === id));
-  if (entityExists) {
-    return arrayUpdate(arr, id, obj, idKey);
+export function arrayUpsert<Root extends any[], Entity extends object[]>(arr: Root, objs: Entity, idKey?: string): Root[0][];
+export function arrayUpsert<Root extends any[]>(arr: Root, id: ID, obj: Partial<Root[0]>, idKey?: string): Root[0][];
+export function arrayUpsert<Root extends any[]>(arr: Root, objsOrId: Partial<Root> | ID, objOrIdKey: Partial<Root[0]> | ID = DEFAULT_ID_KEY, idKey = DEFAULT_ID_KEY): Root[0][] {
+  let updatedRoot = [...coerceArray(arr)];
+  if (isArray(objsOrId)) {
+    idKey = objOrIdKey as string;
+    const objs = objsOrId as Partial<Root>;
+    objs.map((obj) => {
+      const entityIsObject = isObject(obj);
+      if (entityIsObject) {
+        const id = obj[idKey];
+        updatedRoot = upsert(updatedRoot, obj, id);
+      }
+    });
+    return updatedRoot;
   } else {
-    return arrayAdd(arr, entityIsObject ? { ...obj, [idKey]: id } : obj);
+    const obj = objOrIdKey as Partial<Root[0]>;
+    const id = objsOrId as ID;
+    return upsert(updatedRoot, obj, id);
+  }
+
+  function upsert(updatedArr, obj, id) {
+    const entityIsObject = isObject(obj);
+    const entityExists = updatedArr.some((entity) => (entityIsObject ? entity[idKey] === id : entity === id));
+    if (entityExists) {
+      return arrayUpdate(updatedArr, id, obj, idKey);
+    } else {
+      return arrayAdd(updatedArr, entityIsObject ? { ...obj, [idKey]: id } : obj);
+    }
   }
 }
