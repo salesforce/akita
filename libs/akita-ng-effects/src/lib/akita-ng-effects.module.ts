41ec42f4d60e9020f1662f882b9ec472e3b170ba
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-06-30 16:47:05 +0200

    fix(akita-ng-effects): fix duplicate registration of the same effect class

--- libs/akita-ng-effects/src/lib/akita-ng-effects.module.ts
+++ libs/akita-ng-effects/src/lib/akita-ng-effects.module.ts
@@ -1,9 +1,11 @@
-import { Injector, ModuleWithProviders, NgModule, Type } from '@angular/core';
+import { Injector, ModuleWithProviders, NgModule, Type }                                    from '@angular/core';
 import { _FEATURE_EFFECTS, _ROOT_EFFECTS, FEATURE_EFFECT_INSTANCES, ROOT_EFFECT_INSTANCES } from './tokens';
-import { EffectsRootModule } from './effect-root.module';
-import { Actions } from './actions';
-import { EffectsFeatureModule } from './effect-feature.module';
-import { ModuleManager } from './module-manager.service';
+import { EffectsRootModule }                                                                from './effect-root.module';
+import { Actions }                                                                          from './actions';
+import { EffectsFeatureModule }                                                             from './effect-feature.module';
+import { ModuleManager }                                                                    from './module-manager.service';
+
+const registeredEffects = new Map<string, boolean>();
 
 @NgModule({})
 export class AkitaNgEffectsModule {
@@ -56,6 +58,12 @@ export function createEffectInstances(injector: Injector, effectGroups: Type<any
   }
 
   const effectInstances = mergedEffects.map((effect) => {
+    if (registeredEffects.has(effect.name)) {
+      return;
+    }
+    else {
+      registeredEffects.set(effect.name, true);
+    }
     return injector.get(effect);
   });
 
